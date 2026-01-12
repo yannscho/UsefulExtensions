198d8ab75610523a9201d3e1bad7c0e35832ee95
yannscho <yann.scholtes@me.com>
2026-01-12 18:35:03 +0100

    initial commit

--- Package.swift
+++ Package.swift
@@ -5,18 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "UsefulExtensions",
+    platforms: [
+        .iOS(.v18),
+        .macOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UsefulExtensions",
-            targets: ["UsefulExtensions"]
+            targets: ["Date+Extension"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "UsefulExtensions"
+            name: "Date+Extension"
         ),
 
     ]
