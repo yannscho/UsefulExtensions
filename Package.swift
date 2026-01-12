c9500274a4857a67930aece423ce11ce676a7683
yannscho <yann.scholtes@me.com>
2026-01-12 18:41:40 +0100

    updating package

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UsefulExtensions",
-            targets: ["Date+Extension"]
+            targets: ["UsefulExtensions"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Date+Extension"
+            name: "UsefulExtensions"
         ),
 
     ]
