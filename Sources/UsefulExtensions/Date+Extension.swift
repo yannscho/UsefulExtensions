436dac1d303974456ed6445ea43e77539a4edf51
yannscho <yann.scholtes@me.com>
2026-02-21 20:15:53 +0100

    feature: has same component as

--- Sources/UsefulExtensions/Date+Extension.swift
+++ Sources/UsefulExtensions/Date+Extension.swift
@@ -11,6 +11,10 @@ extension Date {
         calendar.isDate(self, inSameDayAs: other)
     }
     
+    public func hasSame(_ component: Calendar.Component, as other: Date) -> Bool {
+        Calendar.current.isDate(self, equalTo: other, toGranularity: component)
+    }
+    
     public func firstDay(of component: Calendar.Component) -> Date {
         switch component {
         case .weekOfYear, .weekOfMonth:
@@ -86,18 +90,3 @@ extension DateFormatter {
         }
     }
 }
-
-/*
- /// First day of the month
- var startOfMonth: Date {
- let calendar = Calendar.current
- let components = calendar.dateComponents([.year, .month], from: self)
- return calendar.date(from: components)!
- }
- 
- /// Last day of the month
- var endOfMonth: Date {
- let calendar = Calendar.current
- return calendar.date(byAdding: DateComponents(month: 1, day: -1), to: startOfMonth)!
- }
- */
